5da80737a633d9f787528d9c72c815bac0469ed5
jamongraham <jamonjg2@illinois.edu>
2025-02-12 09:06:13 +0000

    finished

--- src/driver.cc
+++ src/driver.cc
@@ -3,9 +3,15 @@
 
 #include "functions.hpp"
 
+/*
+  Order of two if blocks on lines 32-46 doesn't account for when the board is full but theres a winner, it only says its a tie when board is full even if there is a winner
+    watchlist of board[][] variable carries a win for x ([1][2], [2][2], [3][2]) but says that its a tie, when x should be winner
+    workaround => checking for winner should come before checking if board is full
+*/
+
 int main() {
   std::vector<std::vector<char>> board;
-  InitializeBoard(board);
+  InitializeBoard(board); //according to watch list in debug, board is always initialized correctly
 
   char current_player = 'X';
   int row = 0, col = 0;
@@ -23,16 +29,17 @@ int main() {
       std::cerr << "Invalid input: Please enter integers." << std::endl;
     }
 
-    if (MakeMove(board, row, col, current_player)) {
-      if (IsBoardFull(board)) {
-        PrintBoard(board);
-        std::cout << "It's a tie!" << std::endl;
-        break;
-      }
+    if (MakeMove(board, row, col, current_player)) { //if player enters out of bounds index (0 or >3), that is checked here in MakeMove()
+      //below two swapped to make sure it checks for winner before declaring a tie
       char winner = CheckWinner(board);
       if (winner != ' ') {
         PrintBoard(board);
         std::cout << "Player " << winner << " wins!" << std::endl;
+        break;
+      }
+      if (IsBoardFull(board)) {
+        PrintBoard(board);
+        std::cout << "It's a tie!" << std::endl;
         break;
       }
       SwitchPlayer(current_player);
--- src/functions.cc
+++ src/functions.cc
@@ -22,15 +22,15 @@ bool MakeMove(std::vector<std::vector<char>>& board,
               int row,
               int col,
               char player) {
-  if (row <= 0 || row > kBoardSize || col <= 0 || col > kBoardSize) {
+  if (row <= 0 || row > kBoardSize || col <= 0 || col > kBoardSize) { //player input begins at 1 and ends at 3, while vector indexes begin at 0 and end at 2
     std::cerr << "Error: Move out of bounds!\n";
     return false;
   }
-  if (board[col][row] != ' ') {
+  if (board[row-1][col-1] != ' ') { //row should be accessed first not col => watchlist with board variable wasn't getting filled row first column second
     std::cerr << "Error: Cell already occupied!\n";
     return false;
   }
-  board[col][row] = player;
+  board[row-1][col-1] = player; //row first then column, and decrease index by 1 to account for user (they input 1-3 not 0-2)
   return true;
 }
 
@@ -38,21 +38,28 @@ char CheckWinner(const std::vector<std::vector<char>>& board) {
   for (unsigned int i = 0; i < kBoardSize; ++i) {
     if (board[i][0] == board[i][1] && board[i][1] == board[i][2] &&
         board[i][0] != ' ') {
-      return board[0][i];
+      return board[i][0]; //returns false info => should be board[i][0] (found through board watchlist after winning 2nd column as X, says 0 won)
     }
   }
 
   for (unsigned int i = 0; i < kBoardSize; ++i) {
     if (board[0][i] == board[1][i] && board[1][i] == board[2][i] &&
         board[0][i] != ' ') {
-      return board[i][0];
+      return board[0][i];
     }
   }
-
+  /*
+  board watchlist says theres diaganol but doesn't check
+  below only checks for one diagonal (left to right) when we also need to check for other diagonal (right to left)
+  */
   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
       board[0][2] != ' ') {
-    return board[0][0];
+    return board[0][2]; //should return board[0][2] not board[0][0] => x's won diagonally but returned that o's won
   }
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
+      board[0][0] != ' ') { //checking for right to left diagonal that was not accounted for originally
+        return board[0][0];
+      }
   return ' ';
 }
 
